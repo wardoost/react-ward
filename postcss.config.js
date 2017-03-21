341bb382af2e39e189fd4b204e18af7d6ce3dde1
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2017-03-21 21:36:06 +1100

    StyleLint

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: [
+    require('stylelint'),
+    require('postcss-reporter'),
     require('autoprefixer')({
       browsers: [
         '>1%',
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
+import 'index.css'
 
 ReactDOM.render(
-  <h1 style={{textAlign: 'center'}}>Hello Ward!</h1>,
+  <h1 className='title'>Hello Ward!</h1>,
   document.getElementById('root')
 )
