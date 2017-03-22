d7226bb3dcd5bf0928eea47fa8827559fd198eeb
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2017-03-22 12:34:17 +1100

    Disable React Devtools in production

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom'
 import App from 'components/App'
 import 'normalize.css/normalize.css'
 
+if (process.env.NODE_ENV === 'production') {
+  // Disable React devtools
+  if (
+    window.__REACT_DEVTOOLS_GLOBAL_HOOK__ &&
+    Object.keys(window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers).length
+  ) {
+    window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers = {}
+  }
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
